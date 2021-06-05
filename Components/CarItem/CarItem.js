89440145c22fe3d10ca15b3cd5b746f645b50b2e
Junaed Rayhan <junaedrayhan@gmail.com>
2021-06-05 23:41:16 +0600

    React Props for Button Component Done with Button Component

--- Components/CarItem/CarItem.js
+++ Components/CarItem/CarItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {View, Text, ImageBackground} from 'react-native';
+import StyleButton from '../StyleButton/StyleButton';
 import styles from './styles';
+
 
 const CarItem = (props) => {
     return(
@@ -10,6 +12,9 @@ const CarItem = (props) => {
             <Text style={styles.title}>Model S</Text>
             <Text style={styles.subtitle}>Starting at $59,420</Text>
             </View>
+
+            <StyleButton type="primary" content={"Custom Order"} onPress={() => {console.warn("custom order pressed")}}></StyleButton>
+            <StyleButton type="secondary" content={"Existing Inventory"} onPress={() => {console.warn("Existing Inventory pressed")}}></StyleButton>
       </View>
     )
 }
---
+++ Components/StyleButton/StyleButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Pressable, View, Text } from 'react-native';
+import styles from './Style';
+
+const StyleButton = (props) => {
+    const {type, content, onPress } = props;
+    const backgroundColor = type === 'primary' ? '#171A20CC' : '#FFFFFFA6';
+    const textColor = type === 'primary' ? '#FFFFFF' : '#171A20';
+    return (
+        <View style={styles.container}>
+            <Pressable style={[styles.button, {backgroundColor: backgroundColor}]} onPress={() => onPress()}>
+            <Text style={[styles.text, {color: textColor}]}>{content}</Text>
+            </Pressable>
+        </View>
+    );
+};
+
+export default StyleButton;
