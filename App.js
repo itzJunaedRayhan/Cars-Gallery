23c7ba241c137f08cc471ab9e6649711719f3d41
Junaed Rayhan <junaedrayhan@gmail.com>
2021-06-06 01:29:23 +0600

    Header Section Done

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View,ImageBackground } from 'react-native';
 import CarItem from './Components/CarItem/CarItem';
 import CarsList from './Components/CarsList/CarsList';
+import Header from './Components/Header/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <CarItem name={"Model 3"} tagLine={"Order Online For"} tagLineCTA={"Touchless Delivery"} image={require('./assets/images/Model3.jpeg')}/> */}
-      
+      <Header/>
       <CarsList/>
       <StatusBar style="auto" />
     </View>
