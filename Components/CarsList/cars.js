3d449918de6464f06525f842c2867ef67c51c290
Junaed Rayhan <junaedrayhan@gmail.com>
2021-06-06 14:36:10 +0600

    Project Done

--- Components/CarsList/cars.js
+++ Components/CarsList/cars.js
@@ -1,20 +1,35 @@
 export default [{
-    name: 'Model S',
+    name: 'Model A',
     tagLine: 'Starting at $69,420',
     image: require('../../assets/images/ModelS.jpeg'),
 }, {
-    name: 'Model 3',
+    name: 'Model B',
     tagLine: 'Order Online for',
     tagLineCTA: 'Touchless Delivery',
     image: require('../../assets/images/Model3.jpeg'),
 }, {
-    name: 'Model X',
+    name: 'Model C',
     tagLine: 'Order Online for',
     tagLineCTA: 'Touchless Delivery',
     image: require('../../assets/images/ModelX.jpeg'),
 }, {
-    name: 'Model Y',
+    name: 'Model D',
     tagLine: 'Order Online for',
     tagLineCTA: 'Touchless Delivery',
     image: require('../../assets/images/ModelY.jpeg'),
+}, {
+    name: 'Model E',
+    tagLine: 'Order Online for',
+    tagLineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/Cars/6.jpg'),
+}, {
+    name: 'Model F',
+    tagLine: 'Order Online for',
+    tagLineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/Cars/7.jpg'),
+}, {
+    name: 'Model G',
+    tagLine: 'Order Online for',
+    tagLineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/Cars/8.jpg'),
 }];
